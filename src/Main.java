33f9b60f3ec337bd085bf29f1d778fd1c9a84833
Устюгов <alexust11@yandex.ru>
2022-07-30 11:23:19 +0700

    поменял текст для вывода на экран

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет разработчику!!!!");
+        System.out.println("Привет разработчику!!!!!!!");
     }
 }
